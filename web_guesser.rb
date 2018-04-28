9a4ef103c8eebdf4bc7b79f7806298c1de394c4a
Sabrina Robinson <sabrinarob@gmail.com>
2018-04-27 21:06:32 -0600

    Finished guessing game.

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,7 +1,23 @@
 require 'sinatra'
 require 'sinatra/reloader'
-# require './views/index'
-
+SECRET_NUM = rand(100)
 get '/' do
-  erb :index, :locals => {number: rand(100)}
+  erb :index, :locals => {:number => SECRET_NUM }
+end
+
+post '/' do
+  guess = params['guess'].to_i
+  feedback = ''
+  if guess == SECRET_NUM
+    feedback = "correct"
+  elsif guess <= SECRET_NUM
+    feedback = "too low"
+  elsif guess >= SECRET_NUM
+    feedback = "too high"
+  end
+  erb :index, :locals => {
+                          :number => SECRET_NUM,
+                          :guess => guess,
+                          :feedback => feedback
+                        }
 end
